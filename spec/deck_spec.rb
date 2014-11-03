ab4fd09a71744146452b7bcd0d7979aebb25081c
John Foley <john@hisfoleyness.com>
2014-11-03 15:28:28 -0800

    rewrite tests for clarity

--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -5,36 +5,53 @@ describe BlackJack::Deck do
     pending("do this spec after Card is complete")
   end
 
-  let(:deck) { BlackJack::Deck.new }
+  describe 'on initialization of a new Deck' do
+    it "has 52 cards in it" do
+      deck = BlackJack::Deck.new
 
-  describe 'on initialization' do
-    it "has 52 cards" do
       expect(deck.size).to be == 52
-      card_types = deck.cards.map{|card| card.class}
-      expect(card_types.uniq).to be == [BlackJack::Card]
+    end
+
+    xit "has only Card objects in it" do
+      deck = BlackJack::Deck.new
+
+      card_classes = deck.cards.map(&:class).uniq
+
+      expect(card_classes.first).to be == BlackJack::Card
+      expect(card_classes.length).to eq(1)
     end
   end
 
-  xdescribe '#shuffle' do
-    it "changes the order of the cards" do
-      original_deck_order = deck.cards.dup
-      shuffled_order = deck.shuffle
-      expect(deck.cards).to be == shuffled_order
+  describe '#shuffle' do
+    xit "changes the order of the cards" do
+      deck = BlackJack::Deck.new
+
+      original_deck = deck.cards
+      shuffled_deck = deck.shuffle
+
+      expect(original_deck).to_not be == shuffled_deck
     end
   end
 
-  xdescribe '#deal' do
-    it "decrements the deck size by 1" do
+  describe '#deal' do
+    xit "decrements the deck size by 1" do
+      deck = BlackJack::Deck.new
+
       deck.deal
       expect(deck.size).to be == 51
     end
 
-    it "returns the card" do
+    xit "returns the card" do
+      deck = BlackJack::Deck.new
+
       expect(deck.deal).to be_instance_of(BlackJack::Card)
     end
 
-    it "returs nil when all the cards have been dealt" do
+    xit "returns nil when all the cards have been dealt" do
+      deck = BlackJack::Deck.new
+
       52.times { deck.deal }
+
       expect(deck.deal).to be == nil
     end
   end
